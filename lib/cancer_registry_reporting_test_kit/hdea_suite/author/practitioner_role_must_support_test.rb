2bbc9ba036ec99a6da0c81fe6f0b96c94339d60a
Nimisha <nimisha.madhusudan@srmtech.com>
2026-01-06 15:22:22 +0530

    Practitioner, DiagnosticReport and Author Resources Updated for CCRR to Support Version 2.0.0-ballot

--- lib/cancer_registry_reporting_test_kit/hdea_suite/author/practitioner_role_must_support_test.rb
+++ lib/cancer_registry_reporting_test_kit/hdea_suite/author/practitioner_role_must_support_test.rb
@@ -33,7 +33,8 @@ module CancerRegistryReportingTestKit
       end
 
       def self.metadata
-        @metadata ||= HdeaGenerator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true))
+        @metadata ||= HdeaGenerator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'practitioner_role_metadata.yml'),
+                                                                      aliases: true))
       end
 
       def scratch_resources
